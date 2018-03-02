59646b41b4b91dde93ac59311b129f3e9e2fe514
MattSegal <mattdsegal@gmail.com>
2018-03-02 23:12:14 +1100

    update prod bucket

--- photos/photos/settings/dev.py
+++ photos/photos/settings/dev.py
@@ -2,9 +2,6 @@ from . import *
 
 ALLOWED_HOSTS = ['192.168.2.2']
 
-# Legacy bucket names
-# 'family-photos-dev-orig'
-# 'family-photos-dev-thumb'
 AWS_STORAGE_BUCKET_NAME = 'family-photos-dev'
 
 # Celery
--- photos/photos/settings/prod.py
+++ photos/photos/settings/prod.py
@@ -3,10 +3,7 @@ from . import *
 DEBUG = False
 ALLOWED_HOSTS = ['memories.ninja', '54.252.159.196']
 
-# Legacy bucket names
-# 'family-photos-prod-orig'
-# 'family-photos-prod-thumb'
-AWS_STORAGE_BUCKET_NAME = 'family-photos-prod'
+AWS_STORAGE_BUCKET_NAME = 'memories-ninja-prod'
 
 DJANGO_SETTINGS_MODULE = 'photos.settings.prod'
 CELERY_TASK_ALWAYS_EAGER = False
