ab5408f6257f43c98f9beff130c5253daae0ca04
Matthew Segal <mattdsegal@gmail.com>
2018-06-11 23:05:25 +1000

    improve admin

--- app/photos/admin.py
+++ app/photos/admin.py
@@ -6,6 +6,8 @@ admin.site.register(Album)
 
 @admin.register(Photo)
 class Photo(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'uploaded_at', 'taken_at')
+
     readonly_fields = (
         'created_at',
         'local_file',
