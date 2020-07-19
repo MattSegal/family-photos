dc4aca17d9a98f09a3cd6cfa6c978ef6db45b6d0
Matthew Segal <mattdsegal@gmail.com>
2020-07-19 16:11:30 +1000

    Preload gunicorn

--- app/scripts/web/prod.sh
+++ app/scripts/web/prod.sh
@@ -6,6 +6,7 @@ echo "Running migrations"
 echo "Starting gunicorn"
 gunicorn photos.wsgi:application \
     --name photos \
+    --preload \
     --workers 1 \
     --threads 2 \
     --bind 0.0.0.0:8000
